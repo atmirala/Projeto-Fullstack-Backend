2760f9df85ed52eaa297396d447b39aed581f24d
atmirala <atmirala@gmail.com>
2020-09-09 15:47:30 -0300

    data e business

---
+++ src/data/UserDatabase.ts
@@ -0,0 +1,40 @@
+import BaseDatabase from "./BaseDatabase";
+import { User } from "../model/UserModel";
+
+export default class UserDatabase extends BaseDatabase {
+    public TABLE_NAME = "fullstack_user";
+
+    public async createUser(
+        id: string,
+        name: string,
+        email: string,
+        nickname: string,
+        password: string
+    ): Promise<void> {
+        try {
+            await this.getConnection()
+            .insert({
+                id,
+                name,
+                email,
+                nickname,
+                password,
+            })
+            .into(UserDatabase.TABLE_NAME)
+        } catch (error) {
+            throw new Error(error.sqlMessage || error.message);
+        }
+       
+    }
+
+    public async getUserByEmail(email:string):Promise<User|void>{
+        const result = await this.getConnection()
+            .select()
+            .from(UserDatabase.TABLE_NAME)
+            .where({email});
+        await BaseDatabase.destroyConnection();
+
+        return User.toUserModel(result[0]);
+        
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,6 @@ app.use(express.json());
 
 dotenv.config();
 
-const connection = knex({
-  client: "mysql",
-  connection: {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT || "3306"),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  },
-});
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
